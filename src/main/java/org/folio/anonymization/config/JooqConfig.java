4044313e9cad75f7e7e8e3ffe784179d3dadec3c
Noah Overcash <novercash@ebsco.com>
2026-05-01 15:09:01 -0400

    Disable triggers and other postgres checks during execution

--- src/main/java/org/folio/anonymization/config/JooqConfig.java
+++ src/main/java/org/folio/anonymization/config/JooqConfig.java
@@ -1,5 +1,11 @@
 package org.folio.anonymization.config;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.jooq.ConnectionProvider;
+import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DataSourceConnectionProvider;
 import org.springframework.boot.jooq.autoconfigure.DefaultConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +22,30 @@ public class JooqConfig {
     // https://github.com/jOOQ/jOOQ/issues/14973#issuecomment-1518061328
     // https://stackoverflow.com/a/74521880/4236490
     return c -> c.settings().withQueryTimeout(QUERY_TIMEOUT_SECONDS);
+  }
+
+  @Bean
+  public ConnectionProvider connectionProvider(DataSource ds) {
+    return new JooqConnectionProvider(ds);
+  }
+
+  public class JooqConnectionProvider extends DataSourceConnectionProvider {
+
+    public JooqConnectionProvider(DataSource dataSource) {
+      super(dataSource);
+    }
+
+    @Override
+    public Connection acquire() {
+      try {
+        Connection conn = super.acquire();
+        // disables triggers/fk checks/etc for this session, speeding things up
+        // and preventing some modules from attempting to update modification timestamps/etc
+        conn.prepareCall("SET session_replication_role = replica").execute();
+        return conn;
+      } catch (SQLException e) {
+        throw new DataAccessException("Something failed", e);
+      }
+    }
   }
 }
