10ea808b242868cdee89cd53f0e74db21eca6f41
Kriti Jain <kjain@ebsco.com>
2026-05-04 12:17:24 -0500

    adding url anon

---
+++ src/main/java/org/folio/anonymization/jobs/URLRedaction.java
@@ -0,0 +1,63 @@
+package org.folio.anonymization.jobs;
+
+import java.util.List;
+import org.folio.anonymization.domain.db.FieldReference;
+import org.folio.anonymization.domain.job.Job;
+import org.folio.anonymization.domain.job.JobBuilder;
+import org.folio.anonymization.domain.job.JobConfigurationProperty;
+import org.folio.anonymization.domain.job.JobFactory;
+import org.folio.anonymization.domain.job.SharedExecutionContext;
+import org.folio.anonymization.domain.job.TenantExecutionContext;
+import org.folio.anonymization.jobs.templates.RedactPart;
+import org.jooq.Condition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class URLRedaction implements JobFactory {
+
+  private static final List<FieldReference> FIELDS = List.of(
+    new FieldReference("agreements", "document_attachment", "da_url"),
+    new FieldReference("agreements", "package_description_url", "pdu_url"),
+    new FieldReference("agreements", "string_template", "strt_rule"),
+    new FieldReference("copycat", "profile", "jsonb", "$.url"),
+    new FieldReference("erm_usage", "usage_data_providers", "jsonb", "$.harvestingConfig.sushiConfig.serviceUrl"),
+    new FieldReference("kb_ebsco_java", "kb_credentials", "url"),
+    new FieldReference("licenses", "document_attachment", "da_url"),
+    new FieldReference("organizations_storage", "contacts", "jsonb", "$.urls[*].value"),
+    new FieldReference("organizations_storage", "organizations", "jsonb", "$.contacts.urls[*].value"),
+    new FieldReference("organizations_storage", "organizations", "jsonb", "$.privilegedContacts.urls[*].value"),
+    new FieldReference("organizations_storage", "interface", "jsonb", "$.uri"),
+    new FieldReference("organizations_storage", "organizations", "jsonb", "$.urls[*].value"),
+    new FieldReference("organizations_storage", "organizations", "jsonb", "$.agreements[*].referenceUrl"),
+    new FieldReference("organizations_storage", "privileged_contacts", "jsonb", "$.urls[*].value")
+  );
+
+  @Autowired
+  private SharedExecutionContext context;
+
+  @Override
+  public List<JobBuilder> getBuilders(TenantExecutionContext tenant) {
+    return List.of(
+      new JobBuilder(
+        "URL redaction",
+        "Redacts URL values by replacing all alphanumeric characters with 'X', 'x', and '1'.",
+        tenant,
+        context,
+        JobConfigurationProperty.fromFieldList(FIELDS, tenant),
+        ctx ->
+          new Job(ctx, List.of("redact"))
+            .scheduleParts(
+              "redact",
+              JobConfigurationProperty
+                .getEnabledFields(ctx.settings())
+                .map(field -> {
+                  Condition condition = field.baseColumn(ctx.tenant().tenant()).isNotNull();
+                  return new RedactPart("Redact " + field.toString(), field, condition);
+                })
+                .toList()
+            )
+      )
+    );
+  }
+}
