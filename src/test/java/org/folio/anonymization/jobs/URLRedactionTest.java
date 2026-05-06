a84d1b445831d722942fd71acb57448e3a1af2e0
Noah Overcash <novercash@ebsco.com>
2026-05-06 12:12:11 -0400

    fix test

--- src/test/java/org/folio/anonymization/jobs/URLRedactionTest.java
+++ src/test/java/org/folio/anonymization/jobs/URLRedactionTest.java
@@ -48,7 +48,7 @@ class URLRedactionTest {
     assertHasLabel(parts, "organizations_storage.contacts.jsonb->'$.urls[*].value'");
     assertHasLabel(parts, "organizations_storage.organizations.jsonb->'$.contacts.urls[*].value'");
     assertHasLabel(parts, "organizations_storage.organizations.jsonb->'$.privilegedContacts.urls[*].value'");
-    assertHasLabel(parts, "organizations_storage.interface.jsonb->'$.uri'");
+    assertHasLabel(parts, "organizations_storage.interfaces.jsonb->'$.uri'");
     assertHasLabel(parts, "organizations_storage.organizations.jsonb->'$.urls[*].value'");
     assertHasLabel(parts, "organizations_storage.organizations.jsonb->'$.agreements[*].referenceUrl'");
     assertHasLabel(parts, "organizations_storage.privileged_contacts.jsonb->'$.urls[*].value'");
